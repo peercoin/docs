94a0aff35960e679964cd8d4bb72dab33bb7c5e2
Kazzkiq <claudiohw@gmail.com>
2017-06-04 21:11:40 -0300

    Scaffold

---
+++ app/initialize.js
@@ -0,0 +1,9 @@
+import Main from './components/main.component';
+
+document.addEventListener('DOMContentLoaded', main);
+
+function main () {
+  window.mainComponent = new Main({
+    target: document.querySelector('main'),
+  });
+}
